29e1b6d9f2826f37006aed7d2b86c363fe3f5d00
Klosekiller <111622444+Klosekiller@users.noreply.github.com>
2023-05-10 15:45:56 -0400

    Should be final

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 document.querySelectorAll(".addTip").forEach( function CalculateTip (button) {
     button.addEventListener('click', () => {
-        let theValue = Number(button.value);
+        let theTip = Number(button.value);
         let theBill = Number(document.getElementById("bill").value);
-        let theTotal = (Number(theBill+(theBill*theValue))).toFixed(2);
+        let theTotal = (Number(theBill+(theBill*theTip))).toFixed(2);
         document.getElementById("total").innerHTML = `$${theTotal}`;
     })  
 });
 document.getElementById("apply").addEventListener("click", function CustomTip () {
     let theBill = Number(document.getElementById("bill").value);
-    let theCustomTip = Number(document.getElementById("custom").value);
-    let theTotal = (Number(theBill+theCustomTip)).toFixed(2);
+    let theTip = Number(document.getElementById("custom").value)/100;
+    let theTotal = Number(theBill+(theBill*theTip)).toFixed(2);
     document.getElementById("total").innerHTML = `$${theTotal}`;
 });
 document.getElementById("reset").addEventListener("click", function Reset () {
