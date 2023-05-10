6bdb34fdbb5d2c58fca52ecb7541b7059470fd8d
Klosekiller <111622444+Klosekiller@users.noreply.github.com>
2023-05-10 15:26:11 -0400

    fixing

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.querySelectorAll(".addTip").forEach( function CalculateTip (button) {
         document.getElementById("total").innerHTML = `$${theTotal}`;
     })  
 });
-document.getElementById("add").addEventListener("click", function CustomTip () {
+document.getElementById("apply").addEventListener("click", function CustomTip () {
     let theBill = Number(document.getElementById("bill").value);
     let theCustomTip = Number(document.getElementById("custom").value);
     let theTotal = (Number(theBill+theCustomTip)).toFixed(2);
