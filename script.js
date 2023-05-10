c572ef31e8c0144b1c9ccbb4b95b840b22b5a023
Klosekiller <111622444+Klosekiller@users.noreply.github.com>
2023-05-10 15:04:42 -0400

    modified

--- script.js
+++ script.js
@@ -6,14 +6,12 @@ document.querySelectorAll(".addTip").forEach( function CalculateTip (button) {
         document.getElementById("total").innerHTML = `$${theTotal}`;
     })  
 });
-
 document.getElementById("add").addEventListener("click", function CustomTip () {
     let theBill = Number(document.getElementById("bill").value);
     let theCustomTip = Number(document.getElementById("custom").value);
     let theTotal = (Number(theBill+theCustomTip)).toFixed(2);
     document.getElementById("total").innerHTML = `$${theTotal}`;
 });
-
 document.getElementById("reset").addEventListener("click", function Reset () {
     document.getElementById("bill").value = 0;
     document.getElementById("custom").value = 0;
